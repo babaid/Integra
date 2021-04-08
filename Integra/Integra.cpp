83679811b0571f32e6ae9e6a9f3c8e0ca7b4117a
Dezsö Babai <dezso.babai@tum.de>
2021-04-08 19:41:42 +0200

    Projektdateien hinzufügen.

---
+++ Integra/Integra.cpp
@@ -0,0 +1,38 @@
+﻿#include "integra.h"
+
+
+double integrate_basic_1d(std::pair<double, double>& bounds, std::function<double(double)>& function)
+{
+	const unsigned int N = 10000;
+	const double epsilon = 1e-4;
+	double step = (bounds.second - bounds.first) / N;
+	double sup_sum{ 0 }, inf_sum{ 0 };
+	for (size_t i = 0; i < N; ++i)
+	{
+		sup_sum += step * function((i + 1) * step);
+		inf_sum += step * function(i * step);
+	}
+
+	if (std::abs(sup_sum - inf_sum) < epsilon) return (inf_sum + sup_sum) / 2;
+	else return -1;
+
+}
+
+double integrate_basic_2d(std::vector<std::pair<double, double>>& bounds, std::function<double(double, double)>& function)
+{
+	const int N = 5000;
+	double epsilon = 1e-3;
+	std::vector<double> steps;
+	for (auto& a : bounds) steps.push_back((a.second - a.first) / N);
+	double sup_sum{ 0 }, inf_sum{ 0 };
+	for (std::size_t i = 0; i < N; i++) {
+		for (std::size_t j = 0; j < N; j++)
+		{
+			sup_sum += steps[0] * steps[1] * function(i * steps[0], j * steps[1]);
+			inf_sum += steps[0] * steps[1] * function((i + 1) * steps[0], (j + 1) * steps[1]);
+		}
+	}
+
+	if (std::abs(sup_sum - inf_sum) < epsilon) return inf_sum;
+	else return -1;
+}
---
+++ Integra/Integra.h
@@ -0,0 +1,40 @@
+﻿#ifndef _INTEGRA_H
+#define _INTEGRA_H
+
+#include<functional>
+#include<concepts>
+#include<utility>
+#include<vector>
+
+
+
+double integrate_basic_1d(std::pair<double, double>& bounds, std::function<double(double)>& function);
+double integrate_basic_2d(std::vector<std::pair<double, double>>& bounds, std::function<double(double, double)>& function);
+class Integral
+{
+
+
+
+};
+
+//template<typename T>
+//concept Number = std::integral<T> || std::floating_point<T>;
+
+template<typename T>
+double multi_integrate(T&&, std::function<double(double ...)> function);
+
+
+#endif
+
+
+
+template<typename T>
+inline double multi_integrate(std::vector<std::pair<double, double>>& bounds, std::function<double(double...)> function)
+{
+	const int N = 1000;
+	std::vector<double> steps;
+	for (auto& a : bounds) steps.push_back((a.first - a.second) / N);
+
+
+	return 0.0;
+}
